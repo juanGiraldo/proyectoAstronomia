af737a626ee3bf75132daedcadeac249f4283ad6
juanGiraldo <jc.giraldo10@uniandes.edu.co>
2016-10-10 22:46:20 -0500

    trabajo en progreso para mostrar la incertidumbre, cambios ligeros al script, datos de Dexter y grafica temporal

--- correlacion.py
+++ correlacion.py
@@ -2,11 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astroML.correlation import two_point
 datos=np.genfromtxt("loadabletxt.csv",usecols=(2,3,4),delimiter=",")
+error=np.genfromtxt("standard correlation.1")
 media = np.median(datos[:,0])
 
 ii = datos[:,0] > media
 HM = datos[ii,1:3]
 LM = datos[~ii,1:3]
+xerr=0.1#error[:,2]
+yerr=0.01#error[:,3]
 print np.shape(HM)
 
 
@@ -15,7 +18,9 @@ cr1=two_point(LM,bins)
 cr2=two_point(HM,bins)
 plt.figure(1)
 plt.plot(cr1,label="masas bajas simulacion")
+plt.scatter(error[:,0],error[:,1])
+#plt.errorbar(error[:,0],error[:,1],xerr=xerr,yerr=yerr)
 plt.plot(cr2,label="masas altas simulacion")
 plt.xscale("log")
 plt.title("correlacion de masas")
-plt.savefig("correlacion_masas.pdf")
+plt.savefig("correlacion_masas_con_error.pdf")
